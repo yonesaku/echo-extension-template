9510813b761664ea53b0371fec3757bc99a2a66e
yonesaku <yonehara.s@yahoo.com>
2025-10-29 10:47:42 +0100

    Update build.gradle.kts

--- ext/build.gradle.kts
+++ ext/build.gradle.kts
@@ -13,8 +13,8 @@ dependencies {
     testImplementation(libs.junit)
     testImplementation(libs.coroutines.test)
     testImplementation(libs.echo.common)
-    implementation("com.squareup.okhttp3:okhttp:4.11.0")
-implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    implementation("com.squareup.okhttp3:okhttp:5.2.1")
+implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
 }
 
 java {
